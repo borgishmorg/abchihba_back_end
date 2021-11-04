aac141ca0b68ac444752e134948349b828faee10
Aleksey Semenov <43957407+borgishmorg@users.noreply.github.com>
2021-11-04 15:28:47 +0200

    feat: add test script

---
+++ test.py
@@ -0,0 +1,38 @@
+import argparse
+import requests
+
+parser = argparse.ArgumentParser(description='Test api')
+parser.add_argument(
+    '--host',
+    metavar='host',
+    default='localhost',
+    action='store',
+    type=str,
+    help='api host'
+)
+parser.add_argument(
+    '--port',
+    metavar='port',
+    default='4000',
+    action='store',
+    type=str,
+    help='api port'
+)
+
+if __name__ == '__main__':
+    args = parser.parse_args()
+    url = f'http://{args.host}:{args.port}'
+
+    print('Send image "image.jpg" to', f'{url}/upload')
+    post_request = requests.post(
+        f'{url}/upload',
+        files={'file': open('image.jpg', 'rb')}
+    )
+    id = post_request.text
+    print('Receive id:', id)
+
+    print('Get image "image_test.jpg" from', f'{url}/get')
+    get_request = requests.get(f'{url}/get', params={'id': id})
+
+    with open('image_test.jpg', 'wb') as f:
+        f.write(get_request.content)
