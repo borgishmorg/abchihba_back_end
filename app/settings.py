cdfbe00ed57a0b6189ab99181b1cd1b7c964613e
Aleksey Semenov <43957407+borgishmorg@users.noreply.github.com>
2021-11-04 15:54:55 +0200

    fix: fix db connection in docker

--- app/settings.py
+++ app/settings.py
@@ -5,7 +5,7 @@ class Settings(BaseSettings):
     PORT: int = 4000
 
     ENABLE_AUTORELOAD: bool = True
-    DB_DSN: str = 'mysql+pymysql://user:password@localhost/db'
+    DB_DSN: str = 'mysql+pymysql://user:63t637gdwuy@localhost/db'
 
 
 settings = Settings()
