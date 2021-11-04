855f59d88df62b026c9282f6cec40645190fcd36
Aleksey Semenov <43957407+borgishmorg@users.noreply.github.com>
2021-11-04 17:25:37 +0200

    feat: add optional scale parameter for get

--- app/api/get_image.py
+++ app/api/get_image.py
@@ -1,4 +1,5 @@
 from io import BytesIO
+from PIL import Image
 from fastapi import Depends, HTTPException, status, Query
 from fastapi.responses import StreamingResponse
 from ..models import ImageDB
@@ -7,6 +8,7 @@ from ..db import session_scope, Session
 
 async def get(
     id: int = Query(...),
+    scale: float = Query(1., gt=0.),
     db: Session = Depends(session_scope)
 ) -> StreamingResponse:
     image_db = db.query(ImageDB).filter(ImageDB.id == id).first()
@@ -17,9 +19,24 @@ async def get(
             detail='Image not found'
         )
 
+    image = Image.open(BytesIO(image_db.data), formats=['JPEG'])
+
+    if scale != 1.:
+        new_width = max(int(image.width * scale), 1)
+        new_height = max(int(image.height * scale), 1)
+
+        image = image.resize((new_width, new_height))
+
     return StreamingResponse(
-        content=BytesIO(image_db.data),
+        content=image_io(image),
         headers={
             'Content-Disposition': f'attachment; filename="{id}.jpg"'
         }
     )
+
+
+def image_io(image: Image.Image) -> BytesIO:
+    image_io = BytesIO()
+    image.save(image_io, format='JPEG')
+    image_io.seek(0)
+    return image_io
